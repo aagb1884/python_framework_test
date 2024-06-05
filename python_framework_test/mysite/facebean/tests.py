30e8e72483645b69a754175929f786b1f6cc2453
aagb1884 <a.a.g.blair@gmail.com>
2024-06-05 06:40:40 +0100

    one test written, passing
    testing if you can post a message

--- python_framework_test/mysite/facebean/tests.py
+++ python_framework_test/mysite/facebean/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Message
+from django.utils import timezone
 
 # Create your tests here.
+
+class MessagePostTests(APITestCase):
+    def test_create_message(self):
+        url = reverse('message-list')
+        data = {'content': 'I cannot wait to serve my country.', 'pub_date': timezone.now(), 'username': 'Cuthbert_CW', 'avatar': 'images/cuthbert.jpg'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Message.objects.count(), 1)
+        self.assertEqual(Message.objects.get().content, 'I cannot wait to serve my country.')
